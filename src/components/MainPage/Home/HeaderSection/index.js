73bcd82de72468aca8d2b9899aa22c3a35843cb5
Raihan Iyai <raihan.suraperwata@tokopedia.com>
2021-03-27 20:53:38 +0700

    implement user api

--- src/components/MainPage/Home/HeaderSection/index.js
+++ src/components/MainPage/Home/HeaderSection/index.js
@@ -19,7 +19,7 @@ const HeaderSection = ({ user }) => {
       <div className={Header}>
         <div>
           <div className={Welcome}>Welcome,</div>
-          <div className={Username}>{user.name}</div> 
+          <div className={Username}>{user.first_name} {user.last_name}</div> 
         </div>
 
         <div>
@@ -30,13 +30,13 @@ const HeaderSection = ({ user }) => {
 
       <div className={WalletContainer}>
         <div className={BalanceContainer}>
-          <div className={Balance}>{user.balance}</div>
+          <div className={Balance}>$ {user.balance}</div>
           <div className={BalanceDescription}>Telego Balance</div> 
         </div>
 
         <div className={BalanceAction}>
           <LogoutOutlined className={RotateIcon} />
-          <MoneyCollectOutlined className={Icon} style={{color: "#1E1E3F"}}/>
+          <MoneyCollectOutlined className={Icon} />
         </div>
       </div>
     </>
--- src/components/MainPage/Home/index.js
+++ src/components/MainPage/Home/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import HeaderSection from './HeaderSection';
 import BodySection from './BodySection';
+import { useFetchUserData } from '../../../helpers/apiGet';
 import { Background, BackgroundContainer, Container, GachaButton, Countdown, HeaderBox} from './style';
 
 const user = {
   name: 'Dharmawan Santosa',
-  balance: '$235.90'
+  balance: '235.90'
 };
 
 const Home = () => {
+  const { loading, response: userData } = useFetchUserData(1);
+
   return (
     <>
     <div className={BackgroundContainer}>
       <div className={Background}></div>
       <div className={HeaderBox}></div>
       <div className={Container}>
-        <HeaderSection user={user} />
+        <HeaderSection user={loading ? user : userData} />
         <BodySection />
         <div className={GachaButton}>
           <div className={Countdown}> 22:58:02 </div>
